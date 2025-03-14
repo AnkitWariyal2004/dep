6826ecd5dc8e2fcda4da13142cacbd1911dcd439
Ankit Wariyal <wariyalankit436@gmail.com>
2025-03-14 17:51:21 +0530

    Initial commit t

--- src/app/api/docupload/[id]/route.js
+++ src/app/api/docupload/[id]/route.js
@@ -1,8 +1,8 @@
-import { connectDB } from "@/lib/db";
-import Documents from "@/models/Document";
+import  dbConnect  from "@/lib/dbConnect";
+import Documents from "@/lib/models/document";
 
 export async function PUT(req, { params }) {
-  await connectDB();
+  await dbConnect();
 
   const { id } = params;
   const body = await req.json();
